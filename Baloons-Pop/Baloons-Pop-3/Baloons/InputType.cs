dbdbf9db812de36b7a687db08269e39ff9e6239d
veronik <veronika.d.georgieva@gmail.com>
2013-05-15 10:07:34 +0300

    Create InputType.cs
    Created new enumeration to check input information

---
+++ Baloons-Pop/Baloons-Pop-3/Baloons/InputType.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BalloonsPops
+{
+    public enum InputType
+    {
+        Coordinates,
+        Command,
+        Invalid
+    }
+}
