4d3725569cf9c1543266c9030074b8f6ecec501c
Veronika Georgieva <veronika.d.georgieva@gmail.com>
2013-05-17 10:48:39 +0300

    Added unit test class
    Added unit test class for PersonScoreComparer.cs

---
+++ Baloons-Pop/Baloons-Pop-3/BalloonTests/TestBalloonPops/TestPersonScoreComparer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using BalloonsPops;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestBalloonPops
+{
+    [TestClass]
+    public class TestPersonScoreComparer
+    {
+        [TestMethod]
+        public void TestCompareTwoPersonWithDifferentScore()
+        {
+            Person firstPerson = new Person("Pesho", 15);
+            Person secondPerson = new Person("Vanya", 10);
+
+            int result = PersonScoreComparer.Compare(firstPerson, secondPerson);
+
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void TestCompareTwoPersonWithTheSameScore()
+        {
+            Person firstPerson = new Person("Pesho", 15);
+            Person secondPerson = new Person("Vanya", 15);
+
+            int result = PersonScoreComparer.Compare(firstPerson, secondPerson);
+
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void TestCompareTwoPersonFirstOneWithTheSmallerScore()
+        {
+            Person firstPerson = new Person("Pesho", 5);
+            Person secondPerson = new Person("Vanya", 15);
+
+            int result = PersonScoreComparer.Compare(firstPerson, secondPerson);
+
+            Assert.AreEqual(-1, result);
+        }
+    }
+}
--- Baloons-Pop/Baloons-Pop-3/Baloons/PersonScoreComparer.cs
+++ Baloons-Pop/Baloons-Pop-3/Baloons/PersonScoreComparer.cs
@@ -7,13 +7,20 @@ namespace BalloonsPops
 {
     public class PersonScoreComparer : IComparer<Person>
     {
-         /// <summary>
+
+        int IComparer<Person>.Compare(Person x, Person y)
+        {
+            // TODO: Implement this method
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
         /// Compares two people by their score.
         /// </summary>
         /// <param name="firstPerson">first person</param>
         /// <param name="secondPerson">second person</param>
         /// <returns></returns>
-        public int Compare(Person firstPerson, Person secondPerson)
+        public static int Compare(Person firstPerson, Person secondPerson)
         {
             return firstPerson.Score.CompareTo(secondPerson.Score);
         }
